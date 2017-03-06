7cccec2f1d9f5460d3e668412575864d6f124f1c
Yamaji-Yuki-Takamatu <ackanabuki02@gmail.com>
2017-03-06 15:48:52 +0900

    Text表示

---
+++ 高松/研修プログラム/山路/TakamatsuLevelUp/TakamatsuLevelUp/main.cpp
@@ -0,0 +1,15 @@
+/**
+* @file
+* @brief 研修用プログラム
+* @author 高松校 山路
+*/
+
+#ifndef INCLUDE_GUARD_STADIO_H
+#include "stdio.h"
+#endif INCLUDE_GUARD_STADIO_H
+
+void main()
+{
+	printf("Hello World");
+	getchar();
+}
