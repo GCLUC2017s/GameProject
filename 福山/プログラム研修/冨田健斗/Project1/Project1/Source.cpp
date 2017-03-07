a3e886dba38417c9e616df5281ff31658b777fce
Tomita-Kento-Fukuyama <16010511@anabuki-net.ne.jp>
2017-03-07 14:04:24 +0900

    プログラム

--- 福山/プログラム研修/冨田健斗/Project1/Project1/Source.cpp
+++ 福山/プログラム研修/冨田健斗/Project1/Project1/Source.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <windows.h>
 #include <math.h>
+const float gravity = 9.8f;
 /*
 ※全て、重力加速度は下方向へ 9.8m / s とする。
 ※全ての問題で空気抵抗は無視する。
@@ -60,11 +61,81 @@ public:
 		}
 	}
 
+	
+	////敵キャラ
+	//struct Enemy
+	//{
+	//	char *name;//名前
+	//	float hp;
+	//	float atttack;
+	//	float defence;
+	//};
+
+	//Enemy enemy_data[] = {
+	//	{ "Enm1", 5.0f, 1.0f, 1.0f },
+	//	{ "Enm2", 5.0f, 3.0f, 1.0f },
+	//	{ "Enm3", 5.0f, 5.0f, 1.0f },
+	//	{ "Enm4", 0.0f, 2.0f, 1.0f },
+	//	{ "Enm5", 2.0f, 2.0f, 1.0f },
+	//	{ "Enm6", 5.0f, 3.0f, 10.0f },
+	//	{ "BOSS", 50.0f, 10.0f, 10.0f } //ボス
+	//};
+
+
+	
+
+};
+
+//座標構造
+struct Vector3
+{
+	float x, y, z;
+};
+
+struct Data
+{
+	Vector3 P0; //初期位置
+	Vector3 P1; //到達位置
+	float time; //経過時間
+
+};
+
+Data question_data[] = {
+	//     p0                        p1               time
+	{ { 2.0f, 3.0f, 4.0f }, { 10.0f, 5.0f, -8.5f }, 5.0f },//1
+	{ { -2.0f, 3.0f, 4.0f }, { 10.0f, 5.0f, -8.5f }, 5.0f },//2
+	{ { 3.0f, 2.0f, 3.0f }, { 1.0f, 2.0f, 3.0f }, 3.5f },//3
+	{ { 4.0f, 2.0f, 3.0f }, { 1.0f, 2.0f, 3.0f }, 3.5f },//4
+	{ { 1.0f, 2.0f, 3.0f }, { 1.0f, -2.0f, 3.0f }, 3.5f },//5
+	{ { 2.0f, 2.0f, 3.0f }, { 1.0f, -2.0f, 3.0f }, 3.5f },//6
+	{ { 3.0f, 2.0f, 3.0f }, { 1.0f, -2.0f, 3.0f }, 3.5f },//7
+	{{ 3.0f, 2.0f, 3.0f }, { -1.0f, -2.0f, -3.0f }, 3.5f },//8
+	{{ 3.0f, -2.0f, 2.0f }, { -1.0f, -2.0f, 3.0f }, 3.5f },//9
+	{{ 3.0f, -2.0f, 1.0f }, { 1.0f, -2.0f, 3.0f }, 3.5f },//10
 };
 
 
 
+// 問題を解くための関数
+Vector3  function(Data data)
+{
+	Vector3 ret_val;
+	ret_val.x = (data.P1.x - data.P0.x) / data.time;
+	ret_val.y = (data.P0.y + gravity / 2 * data.time * data.time - data.P1.y) / data.time;
+	ret_val.z = (data.P1.z - data.P0.z) / data.time;
+	return ret_val;
+	
+};
+
+void toi8(){
+	for (int i = 0; i < 10; ++i) {
+		Vector3 answer = function(question_data[i]);
+		printf("答え (%f,%f,%f)\n ",answer.x,answer.y,answer.z);
+	}
+}
+
 int main(){
+
 	CQuestion question[8];
 
 	printf("Hello world\n\n");
@@ -74,7 +145,7 @@ int main(){
 	question[0].v0 = 5.0f;
 	question[0].t = 2.5f;
 
-	question[0].v = question[0].v0 + -question[0].g * question[0].t;
+	question[0].v = question[0].v0 + question[0].g * question[0].t;
 
 	printf("問1.\n初速度 5.0m / s でボールを放り上げたとき、2.5 秒後のボールの速度は？\n");
 	printf("\n答え %f\n\n", question[0].v);
@@ -141,59 +212,21 @@ int main(){
 
 	question[6].t = 5.0f;
 
-	v0x = (p0x + question[6].g / 2 * question[6].t * question[6].t) / p1x;
-	v0y = (p0y + question[6].g / 2 * question[6].t * question[6].t) / p1y;
-	v0z = (p0z + question[6].g / 2 * question[6].t * question[6].t) / p1z;
+	v0x = (p1x - p0x) / question[6].t; //重力がかからない
 
-	question[6].v0 = v0x + v0y + v0z;
-	question[6].v0 = abs(question[6].v0);
+	v0y = (p0y + -question[6].g / 2 * question[6].t * question[6].t- p1y) / question[6].t; //重力かかる
+
+	v0z = (p1z - p0z) / question[6].t; //重力がかからない
+
 
 	printf("問7.\n３次元空間での問題です。\n位置P0 = (2.0, 3.0, 4.0)\n位置P1 = (10.0, 5.0, -8.5)\nP0からボールを打ち上げて5.0秒後にP1に到達するための\nボールの初速度を求めよ。\n");
-	printf("\n答え %f\n\n", question[6].v0 );
+	printf("\n答え (%f,%f,%f)\n\n", v0x,v0y,v0z );
 	/*終了*/
 
 	/*問8*/
 	printf("問8.\n問7の問題のP0, P1, 経過時間を１０種類用意して、それらの答えが次々に表示されるプログラムを書いてください\n\n");
-	const int x = 0, y = 1, z = 2;
-	for (int i = 0; i < xyz_kazu; i++)
-	{
-		question[7].p0[x][i] = rand() % 21 - 10;
-		question[7].p0[y][i] = rand() % 21 - 10;
-		question[7].p0[z][i] = rand() % 21 - 10;
-
-		question[7].p1[x][i] = rand() % 21 - 10;
-		question[7].p1[y][i] = rand() % 21 - 10;
-		question[7].p1[z][i] = rand() % 21 - 10;
-
-		printf("p0の xは%f,yは%f,zは%f\n", question[7].p0[x][i], question[7].p0[y][i], question[7].p0[z][i]);
-		printf("p1の xは%f,yは%f,zは%f\n", question[7].p1[x][i], question[7].p1[y][i], question[7].p1[z][i]);
-
-		if (question[7].p1[x][i] != 0){
-			v0x = (question[7].p0[x][i] + question[7].g / 2 * question[7].t *question[7].t) / question[7].p1[x][i];
-		}
-		else{
-			v0x = 0;
-		}
-		if (question[7].p1[y][i] != 0){
-			v0y = (question[7].p0[y][i] + question[7].g / 2 * question[7].t *question[7].t) / question[7].p1[y][i];
-		}
-		else{
-			v0y = 0;
-		}
-		if (question[7].p1[z][i] != 0){
-
-			v0z = (question[7].p0[z][i] + question[7].g / 2 * question[7].t *question[7].t) / question[7].p1[z][i];
-		}
-		else{
-			v0z = 0;
-		}
-
-
-		question[7].v0 = v0x + v0y + v0z;
-		question[7].v0 = abs(question[7].v0);
-
-		printf("\n答え %f\n\n", question[7].v0);
-	}
+	
+	toi8();
 
 	/*終了*/
 	getchar();
