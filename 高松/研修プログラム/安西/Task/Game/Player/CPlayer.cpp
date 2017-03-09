2b45ba0132530cb8b8d0967388492727beba6a48
Anzai-Ryoji-Takamatsu <ackanabuki00@gmail.com>
2017-03-09 12:16:47 +0900

    追加

---
+++ 高松/研修プログラム/安西/Task/Game/Player/CPlayer.cpp
@@ -0,0 +1,19 @@
+#include "CPlayer.h"
+
+
+CPlayer::CPlayer()
+{
+	printf("これはCPlayerクラスのコンストラクタです。\n");
+}
+CPlayer::~CPlayer()
+{
+	printf("これはCPlayerクラスのデストラクタです。\n");
+}
+void CPlayer::Update()
+{
+
+}
+void CPlayer::Render()
+{
+	printf("これはCPlayerクラスの描画関数です。\n");
+}
