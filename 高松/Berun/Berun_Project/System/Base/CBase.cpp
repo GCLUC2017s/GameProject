069f2d9a4a6cb80028b9fba3e9d545e6670f4623
Yamaji-Yuki-Takamatu <ackanabuki02@gmail.com>
2017-03-12 01:00:52 +0900

    更新

--- 高松/Berun/Berun_Project/System/Base/CBase.cpp
+++ 高松/Berun/Berun_Project/System/Base/CBase.cpp
@@ -1,17 +1,23 @@
 #include "CBase.h"
 
+//配列数を追加に応じて合わせる
+#define P_ARRAY_SIZE(a)(sizeof(a)/sizeof(a[0]))
+#define E_ARRAY_SIZE(a)(sizeof(a)/sizeof(a[0]))
+
 T_PlayerData g_playerData[] =
 {
 	//ID,レベル,最大HP,現在HP,最大SP,現在SP,攻撃力,防御力,取得経験値,必要経験値,移動速度,ジャンプ力
 	{ 0,1,0,0,0,0,0,0,0,0,0.0f,0.0f },
 	{ 1,1,0,0,0,0,0,0,0,0,0.0f,0.0f },
 };
+#define P_ARRAY_SIZE(g_playerData);
 
 T_EnemyData g_enemyData[] =
 {
 	//ID,最大HP,現在HP,攻撃力,防御力,経験値,移動速度
 	{ 0,0,0,0,0,0,0.0f},
 };
+#define E_ARRAY_SIZE(g_enemyData);
 
 CBase::CBase() : mp_pData(nullptr),mp_eData(nullptr),m_id(0),m_level(0),
 				 m_maxHp(0),m_hp(0),m_maxSp(0),m_sp(0),m_power(0),
