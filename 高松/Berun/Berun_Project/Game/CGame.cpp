db4f1fd5b1358d1dc3935b3ff119cb40034c6901
Yamaji-Yuki-Takamatsu <ackanabuki02@gmail.com>
2017-03-16 13:47:19 +0900

    ゲームシーン修正

--- 高松/Berun/Berun_Project/Game/CGame.cpp
+++ 高松/Berun/Berun_Project/Game/CGame.cpp
@@ -24,25 +24,19 @@ void CGame::Draw()
 	
 }
 /*
-CPlayer CGame::GetPlayer()
+CPlayer CGame::GetPlayer(int type)
 {
 	
-	m_player = new CPlayer();
+	m_player = new CPlayer(type);
 
 	return m_player;
 }
 
-CPlayer CGame::GetEnemy()
+CEnemy CGame::GetEnemy(int type)
 {
-	m_enemy = new CEnemy();
+	m_enemy = new CEnemy(type);
 
 	return m_enemy;
 }
 
-CPlayer CGame::GetMap()
-{
-	m_map = new CMap();
-
-	return m_map;
-}
 */
