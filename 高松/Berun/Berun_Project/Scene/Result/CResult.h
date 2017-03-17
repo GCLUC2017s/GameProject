7644820e71d18ed695d83e105962fc5b9997cbfb
Yamaji-Yuki-Takamatsu <ackanabuki02@gmail.com>
2017-03-17 13:13:07 +0900

    アイテムHeader追加

--- 高松/Berun/Berun_Project/Scene/Result/CResult.h
+++ 高松/Berun/Berun_Project/Scene/Result/CResult.h
@@ -12,10 +12,12 @@
 #include "../CScene.h"
 #include "../Game/Item/CItem.h"
 
+#define IMAGE_MAX 6
 
 class CResult : public CScene
 {
 private:
+	CImage m_image[IMAGE_MAX];
 public:
 	CResult();
 	~CResult();
