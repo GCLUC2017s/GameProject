78fc017fbe961628c9101e62db0c7e357a0f40e5
Anzai-Ryoji-Takamatsu <ackanabuki00@gmail.com>
2017-03-21 09:35:48 +0900

    tuika

---
+++ 高松/Berun/Berun_Project/Game/CollisionManager/CollisionManager.h
@@ -0,0 +1,13 @@
+#ifndef COLLISIONMANAGER_INCLUDE_GUARD
+#define COLLISIONMANAGER_INCLUDE_GUARD
+#include "../CollisionA/CCollisionA.h"
+
+class CCollisionManager
+{
+private:
+public:
+	CCollisionManager();
+	~CCollisionManager();
+};
+
+#endif
