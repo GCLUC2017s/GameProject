540496ed676a04a72fbb120f7ae36c55c68f14cd
Miyata-Kosei-takamatu <ackanabuki01@gmail.com>
2017-03-22 17:32:14 +0900

    敵、ひよこ追加

--- 高松/Berun/Berun_Project/System/CharaBase/CCharaBase.cpp
+++ 高松/Berun/Berun_Project/System/CharaBase/CCharaBase.cpp
@@ -52,8 +52,8 @@ T_AnimData _pigAnimData[] = {
 {
 	{ "LittlePlayerM",0,5,5,5,3,3,0,0,0,1,1,0,0,{ 120,160 } ,_playerMAnimData,{ 550,900 },{ 70,160 },CRect(-50,-160,30,0),eItemMax },
 	{ "LittlePlayerW",1,5,0,0,0,0,0,0,0,1,1,0,0,{ 360,180 },_playerWAnimData,{ 600,300 },{ 135,160 },CRect(-35,-160,45,0),eItemMax },
-	{ "Carrot",2,5,5,5,0,0,0,0,0,1,0,0,1,{160,160} ,_carrotAnimData,{ 160,160 },{ 60,160 },CRect(-60,-160,60,0),eCarrotItem },
-	{ "Chick",3,5,0,0,0,0,0,0,0,1,0,0,1,{ 160,160 } ,_chickAnimData,{ 220,220 },{ 60,160 },CRect(-60,-160,60,0),eCarrotItem },
+	{ "Carrot",2,0,5,5,0,0,1,0,1,0,0,0,1,{160,160} ,_carrotAnimData,{ 160,160 },{ 60,160 },CRect(-60,-160,60,0),eCarrotItem },
+	{ "Chick",3,0,7,7,0,0,0,2,5,0,0,0,1,{ 160,160 } ,_chickAnimData,{ 220,220 },{ 60,160 },CRect(-60,-160,60,0),eCarrotItem },
 	{ "Fish",4,5,0,0,0,0,0,0,0,1,0,0,1,{ 160,160 } ,_fishAnimData,{ 200,200 },{ 60,160 },CRect(-60,-160,60,0),eCarrotItem },
 	{ "Pig",4,5,0,0,0,0,0,0,0,1,0,0,1,{ 160,160 } ,_pigAnimData,{ 220,220 },{ 60,160 },CRect(-60,-160,60,0),eCarrotItem },
 	//{ "Fish",4,5,0,0,0,0,0,0,0,1,0,0,0 },
