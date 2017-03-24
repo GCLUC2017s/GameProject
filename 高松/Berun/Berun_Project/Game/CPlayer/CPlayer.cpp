d2ae20c3099af9df90da8a13029dd9d1dbbec62f
Anzai-Ryoji-Takamatsu <ackanabuki00@gmail.com>
2017-03-24 12:37:43 +0900

    当たり判定更新

--- 高松/Berun/Berun_Project/Game/CPlayer/CPlayer.cpp
+++ 高松/Berun/Berun_Project/Game/CPlayer/CPlayer.cpp
@@ -75,11 +75,6 @@ void CPlayer::Contlol() {
 		if (HOLD_KEY_DOWN)	m_down = true;
 		if (HOLD_KEY_LEFT)	m_left = true;
 		if (HOLD_KEY_RIGHT)	m_right = true;
-		//ジャンプ中、ダメージ中以外に何らかの移動キーを入力していれば、状態を歩きに設定する処理
-		if (m_state != eState_Jump && m_state != eState_Damage)
-		{
-			if (HOLD_KEY_UP || HOLD_KEY_DOWN || HOLD_KEY_LEFT || HOLD_KEY_RIGHT)	m_state = eState_Move;
-		}
 		//シフトキーを入力していれば、状態をダッシュに設定する処理
 		if (HOLD_KEY_SHIFT)	m_dash = true;
 		else m_dash = false;
